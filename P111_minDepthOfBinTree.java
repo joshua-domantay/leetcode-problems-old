a0e9ee1845ff197d0be96151a2f73b70d49e9504
Joshua Anthony Domantay <dom.josh2701@gmail.com>
2021-11-03 03:39:46 -0700

    Finished Problem 111 - Minimum Depth of Binary Tree

---
+++ P111_minDepthOfBinTree.java
@@ -0,0 +1,59 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class P111_minDepthOfBinTree {
+    public static void main(String[] args) {
+        TreeNode left = new TreeNode(2, null, new TreeNode(1));
+        TreeNode right = new TreeNode(4);
+        left = new TreeNode(7, left, right);
+        right = new TreeNode(12, new TreeNode(5), new TreeNode(6));
+        TreeNode head = new TreeNode(10, left, right);
+        /*
+         *         10
+         *       /    \
+         *      7      12
+         *     / \    /  \
+         *    2   4  5    6
+         *     \
+         *      1
+         * 
+         *  2 1 7 4 10 5 12 6
+         */
+
+        inorder(head);
+        System.out.println();
+        System.out.println(minDepth(head));
+    }
+
+    public static int minDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        if((root.left != null) && (root.right != null)) {
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+        if(root.left != null) {
+            return minDepth(root.left) + 1;
+        }
+        return minDepth(root.right) + 1;
+    }
+
+    public static void inorder(TreeNode node) {
+        if(node != null) {
+            inorder(node.left);
+            System.out.print(node.val + " ");
+            inorder(node.right);
+        }
+    }
+}
+
